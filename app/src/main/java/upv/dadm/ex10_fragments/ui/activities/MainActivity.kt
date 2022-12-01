2133af7cb86e02694026bcbdefecac6923615bb8
ddandres <ddandres@disca.upv.es>
2022-12-01 18:14:15 +0100

    Initial commit

---
+++ app/src/main/java/upv/dadm/ex10_fragments/ui/activities/MainActivity.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022
+ * David de Andrés and Juan Carlos Ruiz
+ * Development of apps for mobile devices
+ * Universitat Politècnica de València
+ */
+
+package upv.dadm.ex10_fragments.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import upv.dadm.ex10_fragments.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
